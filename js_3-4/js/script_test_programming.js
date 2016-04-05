fc5ed8054a4547abcc2417bfc08c312735f50299
master-lily <losiklily@gmail.com>
2016-04-05 23:44:43 +0300

    homework

--- js_3-4/js/script_test_programming.js
+++ js_3-4/js/script_test_programming.js
@@ -3,57 +3,98 @@
  */
 
 var test = {
+    testBody: document.body,
     testWrapper: document.createElement('div'),
     testHeader: document.createElement('h1'),
     testContent: document.createElement('content'),
     testButton: document.createElement('button'),
+
+
 
     createTest: function () {
         this.testWrapper.classList.add('wrapper');
         this.testHeader.classList.add('test-tittle');
         this.testButton.classList.add('btn');
 
-        this.testWrapper = document.querySelector('.wrapper');
+        /*this.testWrapper = document.querySelector('.wrapper');* - почему с этой строчкой не работает???*/
 
+        this.testHeader.innerHTML = 'Тест по программированию';
+        this.testButton.innerHTML = 'Проверить мои результаты';
+
+        this.testBody.appendChild(this.testWrapper);
         this.testWrapper.appendChild(this.testHeader);
         this.testWrapper.appendChild(this.testContent);
         this.testWrapper.appendChild(this.testButton);
         this.testButton.setAttribute('value', 'submit');
-        this.testButton.innerHTML = 'Проверить мои результаты';
-
-        this.testHeader.innerHTML = 'Тест по программированию';
     },
 
     createTestQuestion: function () {
-        this.questionTittle = document.createElement('h2');
-        this.questionCheckbox = document.createElement('checkbox');
-        this.answerText = document.createElement('text');
 
-        this.questionTittle.classList.add('question-tittle');
-        this.questionCheckbox.classList.add('checkboxes');
-        this.answerText.classList.add('answer-text');
 
         var question = 1;
 
-        for (var i = 1; i < 3; i++) {
-            this.testContent.appendChild(this.questionTittle);
-            this.questionTittle.innerHTML = i + 'Вопрос №' + i;
+        for (var i = 1; i < 4; i++) {
+            this.questionWrapper = document.createElement('div');
+            this.questionTittle = document.createElement('h2');
+            this.questionForm = document.createElement ('form');
 
-           this.questionTittle.appendChild(this.questionCheckbox);
+            this.questionWrapper.classList.add('question-wrapper');
+            this.questionTittle.classList.add('question-tittle');
+            this.questionForm.classList.add('question-form');
 
-            this.questionCheckbox.appendChild(this.answerText);
+            this.questionTittle.innerHTML = i + '. Вопрос №' + i;
 
-            this.answerText.innerHTML = 'Вариант ответа №' + i;
+            this.testContent.appendChild(this.questionWrapper);
+            this.questionWrapper.appendChild(this.questionTittle);
+            this.questionTittle.appendChild(this.questionForm);
 
-            question++;
+            this.questionForm.setAttribute('action', '#');
+
+
+            for (var j=1; j < 4; j++) {
+
+                this.input = document.createElement('input');
+                this.label = document.createElement('label');
+                this.answerText = document.createElement('text');
+
+                this.label.setAttribute('for', 'question' + question);
+                this.input.setAttribute('type', 'checkbox');
+                this.input.setAttribute( 'id', 'question' + question);
+
+                this.answerText.innerHTML = 'Вариант ответа № ' + j;
+
+                this.input.classList.add('input');
+                this.answerText.classList.add('answer-text');
+                this.questionForm.appendChild(this.input);
+                this.questionForm.appendChild(this.label);
+                this.label.appendChild(this.answerText);
+
+                question++;
+            }
+
+        }
 
     }
 
-    }
 
 };
 
 test.createTest();
 test.createTestQuestion();
+
+
+
+
+
+
+
+/*tittle: "Вопрос",
+ answer: [
+ {text: "Вариант ответа №1", correct: false},
+ {text: "Вариант ответа №2", correct: false},
+ {text: "Вариант ответа №3", correct: false}
+
+ ],*/
+
 
 
